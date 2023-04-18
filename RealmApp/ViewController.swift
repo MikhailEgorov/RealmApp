287a89214d583fa37a1c2888d9fb17e1c39ee30c
MikhailEgorov <egorovmihail25@gmail.com>
2023-04-18 18:41:10 +0300

    Initial Commit

---
+++ RealmApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmApp
+//
+//  Created by Mikhail Egorov on 18.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
